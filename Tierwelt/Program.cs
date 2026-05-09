85c0af6c968b15bd884816d7ba7f1d987e4a9b5c
Mohamed Ali <mohamed.derki@gmail.com>
2026-05-09 21:37:34 +0200

    Updated Program.cs

--- Tierwelt/Program.cs
+++ Tierwelt/Program.cs
@@ -1,9 +1,13 @@
+using System;
+
 namespace Tierwelt
 {
-  internal class Program
-  {
-    public static void Main(string[] args)
+    internal class Program
     {
+        public static void Main(string[] args)
+        {
+            Console.WriteLine("Welcome to Tierwelt!");
+            Console.WriteLine("Git and GitHub project.");
+        }
     }
-  }
 }
